9eb164425eb629556ab9f21bf2656786adbbae83
maaz-official <muhammadmaaz07@gmail.com>
2024-10-06 12:33:00 -0700

    DB Changes

--- config/config.js
+++ config/config.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables
 
 const config = {
+    //You Guys Enter Your Own DB Link Here
     mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/recipe-apps', // Fallback for local development
 };
 
